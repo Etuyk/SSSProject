5a82a5eb7c830db07c198d04ef2b28047f25c9e3
Saviour <saviour.buttigieg.d102681@mcast.edu.mt>
2025-03-23 18:37:22 +0000

    Created models and started implementing college controller

--- assignment_app/app/Http/Controllers/StudentController.php
+++ assignment_app/app/Http/Controllers/StudentController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\College;
+use App\Models\Student;
 
 use Illuminate\Http\Request;
 
@@ -8,36 +10,75 @@ class StudentController extends Controller
 {
     
     public function index() {
-        // List all students, optionally filter by college
+        $colleges = College::orderBy('name')->pluck('name', 'id')->prepend('All Colleges', '');
+        return view('students.index', compact('students', 'colleges'));
+
     }
 
     
     public function create() {
-        // Show form to create student
+        $student = new Student();
+        $colleges = College::orderBy('name')->pluck('name', 'id')->prepend('Select a College', '');
+    
+        return view('students.create', compact('student', 'colleges'));
     }
 
     
     public function store(Request $request) {
-        // Save new student
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'phone' => 'required|regex:/^(\+?\d{1,3}[- ]?)?\d{10}$/',
+            'dob' => 'required|date',
+            'college_id' => 'required|exists:colleges,id'
+        ]);
+    
+        Student::create($request->all());
+    
+        return redirect()->route('students.index')->with('message', 'Student created successfully.');
     }
 
     
     public function show(string $id) {
-        // Show student details
+
+        $student = Student::find($id);
+
+        return view('students.show', compact('student'));
+        
     }
 
     
-    public function edit(string $id) {
-        // Show form to edit student
+    public function edit(string $id){
+
+        $student = Student::findOrFail($id);
+
+        $colleges = College::orderBy('name')->pluck('name', 'id')->prepend('Select a College', '');
+
+        return view('students.edit', compact('student', 'colleges'));
     }
 
    
     public function update(Request $request, string $id) {
-        // Update student
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'phone' => 'required|regex:/^(\+?\d{1,3}[- ]?)?\d{10}$/',
+            'dob' => 'required|date',
+            'college_id' => 'required|exists:colleges,id'
+        ]);
+
+        $student = Student::find($id);
+
+        $student->update($request->all());
+
+        return redirect()->route('students.index')->with('message', 'Student updated successfully.');
     }
 
     
     public function destroy(string $id) {
-        // Delete student
+        $student = Student::find($id);
+        $student->delete();
+
+        return redirect()->route('students.index')->with('message', 'Student deleted successfully.');
     }
 }
