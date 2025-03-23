fee2aab95bdd50c21c9aa23f93fe4295db32740a
Saviour <saviour.buttigieg.d102681@mcast.edu.mt>
2025-03-23 16:43:11 +0000

    Updated Controllers

--- assignment_app/app/Http/Controllers/CollegeController.php
+++ assignment_app/app/Http/Controllers/CollegeController.php
@@ -6,19 +6,41 @@ use Illuminate\Http\Request;
 
 class CollegeController extends Controller
 {
-    public function index()
-{
-    // Show all colleges
-}
+    //Display all colleges
+    public function index() { 
 
-public function create()
-{
-    // Return form to create new college
-}
+    }
 
-public function edit(string $id)
-{
-    // Return form to edit college
-}
+    //Create a new college
+    public function create() { 
 
+    }
+
+    //Store the form data
+    public function store(Request $request) { 
+
+    }
+
+    //Display college detail
+    public function show(string $id) {
+
+     }
+
+
+    //Display edit form
+    public function edit(string $id) {
+
+     }
+
+
+    //Update the college details from the edit form
+    public function update(Request $request, string $id) {
+
+     }
+
+
+    //Destroy the college with the id
+    public function destroy(string $id) {
+
+     }
 }
--- assignment_app/app/Http/Controllers/StudentController.php
+++ assignment_app/app/Http/Controllers/StudentController.php
@@ -6,20 +6,38 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
+    
     public function index() {
         // List all students, optionally filter by college
     }
+
     
     public function create() {
         // Show form to create student
     }
+
+    
+    public function store(Request $request) {
+        // Save new student
+    }
+
+    
+    public function show(string $id) {
+        // Show student details
+    }
+
     
     public function edit(string $id) {
         // Show form to edit student
     }
+
+   
+    public function update(Request $request, string $id) {
+        // Update student
+    }
+
     
     public function destroy(string $id) {
         // Delete student
     }
-    
 }
