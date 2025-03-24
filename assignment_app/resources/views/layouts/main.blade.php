9f30585dacf14a30b9fbaaf4d80cd40dff67efab
Saviour <saviour.buttigieg.d102681@mcast.edu.mt>
2025-03-24 21:29:00 +0000

    Created main.blade layout

---
+++ assignment_app/resources/views/layouts/main.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Colleges</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand text-uppercase" href="{{ route('students.index') }}">            
+            <strong>Colleges</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggler" aria-controls="navbar-toggler" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+            
+        <div class="collapse navbar-collapse" id="navbar-toggler">
+          <ul class="navbar-nav">
+            <li class="nav-item"><a href="{{ route('colleges.index') }}" class="nav-link">Colleges</a></li>
+            <li class="nav-item"><a href="{{ route('students.index') }}" class="nav-link">Students</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+      @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
